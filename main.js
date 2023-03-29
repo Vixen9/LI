1d713353f8ccc0bcb2842fa9b85c083c6f4517fe
Vixen9 <car.ana9@yahoo.com>
2023-03-29 20:53:20 +0300

    js and css code

--- main.js
+++ main.js
@@ -4,8 +4,17 @@ const litext = document.querySelector('#input')
 
 btn.addEventListener('click', function () {
     const li = document.createElement('li')
+    const button = document.createElement('button')
+    const i = document.createElement('i')
+
+    i.classList.add("fa-solid", "fa-xmark")
+
+
     li.innerText = litext.value
     list.append(li)
+    li.append(button)
+    button.append(i)
 
     litext.value = ''
 })
+
